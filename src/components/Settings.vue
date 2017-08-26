5cf3206bab7b0a1449a1816959e6930b7805ac39
Dan Laush <dan.laush@gmail.com>
2017-08-27 00:54:13 +1000

    my god it's a vue app - add streams, toggle menu

---
+++ src/components/Settings.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="settings">
+    <h2>The Menu</h2>
+    <ul>
+      <li v-for="(stream, index) in streams"
+      :key="index">
+      <input type="text" :value="stream" />
+    </li>
+  </ul>
+  <form v-on:submit="addVideoStream">
+    <input id="newStream" />
+    <button id="add" type="submit">Add Stream</button>
+  </form>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'settings',
+    props: ['streams'],
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      };
+    },
+    methods: {
+      addVideoStream(e) {
+        e.preventDefault();
+        this.$emit('addVideoStream', e.target[0].value);
+        e.target.reset();
+      },
+    },
+  };
+</script>
+
+<style scoped>
+input {
+  width: 80%;
+}
+</style>
---
+++ src/components/VideoStream.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="video">
+		<div class="video__wrapper">
+			<iframe type="text/html" width="640" height="360" :src="stream" frameborder="0"></iframe>	
+		</div>	
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'video-stream',
+  props: ['stream'],
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.video {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	overflow: hidden;
+	z-index: 1;
+	transition: transform 400ms;
+}
+
+.video__wrapper {
+	position: relative;
+	padding-bottom: 56.25%; /* 16:9 */
+	/*padding-top: 25px;*/
+	height: 0;
+}
+.video__wrapper iframe {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+</style>
